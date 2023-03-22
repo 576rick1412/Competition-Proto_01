3601a78e5ada91a9e2a9e15da5f92a181e085be6
576rick1412 <576rick1412@gmail.com>
2023-03-23 01:35:07 +0900

    적 기초적인 부분 구현

--- Assets/CS/Airship/Enemy.cs
+++ Assets/CS/Airship/Enemy.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Airship
 {
-    [Header("АјАн")]
-    public string attackType;
+    protected override void Awake()
+    {
+        base.Awake();
+    }
 
-
-
-    void Start()
+    protected override void Start()
     {
         
     }
 
-    void Update()
+    protected override void Update()
     {
         
     }
